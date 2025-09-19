6989933bcf66e88946220319ce910c4d0fc65785
Guilherme-Pc <guilherme.bassani07@gmail.com>
2025-09-19 16:26:28 -0300

    finalizando

--- src/Components/Global/Banner.jsx
+++ src/Components/Global/Banner.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Banner = ({imagem, titulo, texto}) => {
   return (
-    <section className='text-white relative mb-16'>
+    <section className='relative flex flex-col text-white mb-16 justify-items-end'>
       <div className='inset-0 bg-gradient-to-t from-black to-transparent z-20 absolute'></div>
       <img src={imagem} alt="banner de inicio" className=' w-full h-auto object-cover' />
-      <div className='container mx-auto relative bottom-10 z-40 gap-4'>
-        <h1 className='text-7xl font-semibold font-serif absolute bottom-10  max-lg:text-6xl'>{titulo}</h1>
-        <p className='text-white text-2xl max-lg:text-xl'>{texto}</p>
+      <div className='container absolute bottom-0 left-0 max-lg:px-4 z-40 max-md:p-4 md:p-12 '>
+        <h1 className='text-7xl font-semibold font-serif max-md:text-5xl   max-lg:text-6xl'>{titulo}</h1>
+        <p className='text-white text-2xl max-lg:text-xl max-md:text-lg'>{texto}</p>
       </div>
     </section>
   )

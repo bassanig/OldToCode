3aa2850a95631db9bbfeee2f0a29a8e986e77b7d
Guilherme-Pc <guilherme.bassani07@gmail.com>
2025-09-19 16:06:58 -0300

    adicionando oque mudei kaka

--- src/Components/Global/Banner.jsx
+++ src/Components/Global/Banner.jsx
@@ -4,10 +4,10 @@ const Banner = ({imagem, titulo, texto}) => {
   return (
     <section className='text-white relative mb-16'>
       <div className='inset-0 bg-gradient-to-t from-black to-transparent z-20 absolute'></div>
-      <img src={imagem} alt="banner de inicio" className=' w-full  object-cover' />
-      <div className='container mx-auto relative bottom-10 z-40'>
-        <h1 className='text-7xl font-semibold font-serif absolute bottom-10  '>{titulo}</h1>
-        <p className='text-white text-2xl'>{texto}</p>
+      <img src={imagem} alt="banner de inicio" className=' w-full h-auto object-cover' />
+      <div className='container mx-auto relative bottom-10 z-40 gap-4'>
+        <h1 className='text-7xl font-semibold font-serif absolute bottom-10  max-lg:text-6xl'>{titulo}</h1>
+        <p className='text-white text-2xl max-lg:text-xl'>{texto}</p>
       </div>
     </section>
   )

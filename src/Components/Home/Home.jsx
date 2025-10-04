097991cfe6f564fb5d568762026c736336ea1d76
Guilherme-Pc <guilherme.bassani07@gmail.com>
2025-10-03 21:24:10 -0300

    refactor(scroll): adiciona animação ao scroll

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,22 +4,31 @@ import Conhecanos from './Conhecanos'
 import bannerInicio from '../../assets/banners/bannerInicio.png'
 import { useTranslation } from 'react-i18next'
 import Biografias from './biografias2/Biografias'
-import Footer from '../Global/Footer'
 import Destaques from './destaques/Destaques'
 import Fundamentos from './fundamentos/Fundamentos'
 import Cronologia from './cronologia/Cronologia'
+import ScrollAnimation from '../Global/ScrollAnimation'
 
 const Home = () => {
   const { t } = useTranslation();
   return (
     <>
       <Banner imagem={bannerInicio} titulo={'OldToCode'} texto={t('home.banner.subtitle')} />
-      <Conhecanos />
-      <Destaques />
-      <Biografias />
-      <Fundamentos />
-      <Cronologia />
-
+      <ScrollAnimation direction="up">
+        <Conhecanos />
+      </ScrollAnimation>
+      <ScrollAnimation direction="left">
+        <Destaques />
+      </ScrollAnimation>
+      <ScrollAnimation direction="right">
+        <Biografias />
+      </ScrollAnimation>
+      <ScrollAnimation direction="left">
+        <Fundamentos />
+      </ScrollAnimation>
+      <ScrollAnimation direction="right">
+        <Cronologia />
+      </ScrollAnimation>
     </>
   )
 }

059018ef97c510078ed2863a65096c26e145e192
Guilherme-Pc <guilherme.bassani07@gmail.com>
2025-10-04 14:45:54 -0300

    refact(design): melhora o design da header

--- src/Components/Global/Header/FundamentosMenu.jsx
+++ src/Components/Global/Header/FundamentosMenu.jsx
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import conteudos from '../../../data/conteudos.json';
 import TituloTexto from '../TipografiaPaginas/TituloTexto';
-import fotoAlgoritmos from '../../../assets/fotos/fundamentos/algoritmos2.png'
+import fotoAlgoritmos from '../../../assets/fotos/fundamentos/algoritmos2.png';
+import useOutsideClick from '../../Hooks/useOutsideClick';
 
 const FundamentosMenu = ({ setShowFundamentos }) => {
   const { t, i18n } = useTranslation();
+  const menuRef = useRef(null);
 
-  const fundamentosKeys = Object.keys(conteudos).filter(key => key.startsWith('fundamentos-'));
+  useOutsideClick(menuRef, () => setShowFundamentos(false));
+
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, []);
+
+  const fundamentosKeys = Object.keys(conteudos).filter(key =>
+    key.startsWith('fundamentos-'),
+  );
 
   return (
-    <div className="w-full bg-gray-50 absolute z-30 dark:bg-dark animate-slideDown py-8 flex justify-center">
-      <div className=' mx-auto flex max-w-fit gap-8'>
-        <div className='flex flex-col justify-start'>
-          <h1 className='text-5xl text-vermelho font-serif font-bold mb-4 dark:text-amarelo-dark '>{t('header.nav.fundamentals')}</h1>
+    <div
+      ref={menuRef}
+      className="w-full bg-gray-50 absolute z-30 dark:bg-dark animate-slideDown py-8 flex justify-center"
+    >
+      <div className=" mx-auto flex max-w-fit gap-8">
+        <div className="flex flex-col justify-start">
+          <h1 className="text-5xl text-vermelho font-serif font-bold mb-4 dark:text-amarelo-dark ">
+            {t('header.nav.fundamentals')}
+          </h1>
           <ul className="flex flex-col gap-4 py-2">
             {fundamentosKeys.map(key => (
               <li key={key}>
@@ -23,31 +41,61 @@ const FundamentosMenu = ({ setShowFundamentos }) => {
                   className="block px-1 py-2 text-lg text-dark dark:text-gray-200 hover:text-amarelo dark:hover:text-amarelo"
                   onClick={() => setShowFundamentos(false)}
                 >
-                  {conteudos[key][i18n.language]?.titulo || conteudos[key]['pt'].titulo}
+                  {conteudos[key][i18n.language]?.titulo ||
+                    conteudos[key]['pt'].titulo}
                 </NavLink>
               </li>
             ))}
           </ul>
         </div>
-        <div className='flex flex-col relative gap-4 pb-4 px-6'>
-          <h1 className='text-xl text-vermelho pt-3 dark:text-amarelo-dark'>{t('others')}</h1>
-          <button onClick={() => setShowFundamentos(false)} className='absolute cursor-pointer top-3 right-0 w-9 h-9 rounded-full border-2 dark:border-amarelo-dark border-vermelho flex items-center justify-center'>
-            <span className='absolute w-5 h-0.5 bg-vermelho rounded-2xl -rotate-45 dark:bg-amarelo-dark'></span>
-            <span className='absolute w-5 h-0.5 bg-vermelho rounded-2xl rotate-45 dark:bg-amarelo-dark'></span>
-          </button>
-          <div className='flex gap-4 pt-4'>
-            <NavLink to='/conteudo/fundamentos-logica' className='overflow-hidden block max-w-[400px] rounded-2xl relative border-gray-50 shadow-md dark:shadow-2xl'>
-              <img src={fotoAlgoritmos} alt="" className='object-cover h-full w-full'/>
-              <div className='absolute flex flex-col gap-2 bg-gray-50 dark:bg-dark bottom-0 left-0 w-full px-6 py-4 border-6 border-gray-50 dark:border-dark'>
-                <h1 className='text-2xl dark:text-gray-50'>{t('home.logicFundamentals.logic.title')}</h1>
-                <span className='text-md dark:text-gray-50'>{t('home.logicFundamentals.logic.description')}</span>
+        <div className="flex flex-col relative px-6">
+          <div className="flex items-center justify-between pb-4">
+            <h1 className="text-xl text-vermelho pt-3 dark:text-amarelo-dark">
+              {t('others')}
+            </h1>
+            <button
+              onClick={() => setShowFundamentos(false)}
+              className=" cursor-pointer dark:hover:bg-vermelho hover:bg-amarelo-dark trasition duration-150 right-0 w-9 h-9 rounded-full border dark:border-amarelo-dark border-vermelho flex items-center justify-center"
+            >
+              <span className="absolute w-5 h-0.5 bg-vermelho rounded-2xl -rotate-45 dark:bg-amarelo-dark"></span>
+              <span className="absolute w-5 h-0.5 bg-vermelho rounded-2xl rotate-45 dark:bg-amarelo-dark"></span>
+            </button>
+          </div>
+          <div className="flex gap-4 ">
+            <NavLink
+              to="/conteudo/fundamentos-logica"
+              className="overflow-hidden block max-w-[400px] rounded-2xl relative border-gray-50"
+            >
+              <img
+                src={fotoAlgoritmos}
+                alt=""
+                className="object-cover h-full w-full"
+              />
+              <div className="absolute flex flex-col gap-2 bg-gray-50 dark:bg-dark bottom-0 left-0 w-full px-6 py-4 border-6 border-gray-50 dark:border-dark">
+                <h1 className="text-2xl dark:text-gray-50">
+                  {t('home.logicFundamentals.logic.title')}
+                </h1>
+                <span className="text-md dark:text-gray-50">
+                  {t('home.logicFundamentals.logic.description')}
+                </span>
               </div>
             </NavLink>
-            <NavLink to='/conteudo/fundamentos-dados' className='overflow-hidden block max-w-[400px] rounded-2xl relative border-gray-50 shadow-md dark:shadow-2xl'>
-              <img src={fotoAlgoritmos} alt="" className='object-cover h-full w-full'/>
-              <div className='absolute flex flex-col gap-2 bg-gray-50 dark:bg-dark bottom-0 left-0 w-full px-6 py-4 border-6 border-gray-50 dark:border-dark'>
-                <h1 className='text-2xl dark:text-gray-50'>{t('home.logicFundamentals.data.title')}</h1>
-                <span className='text-md dark:text-gray-50'>{t('home.logicFundamentals.data.description')}</span>
+            <NavLink
+              to="/conteudo/fundamentos-dados"
+              className="overflow-hidden block max-w-[400px] rounded-2xl relative border-gray-50"
+            >
+              <img
+                src={fotoAlgoritmos}
+                alt=""
+                className="object-cover h-full w-full"
+              />
+              <div className="absolute flex flex-col gap-2 bg-gray-50 dark:bg-dark bottom-0 left-0 w-full px-6 py-4 border-6 border-gray-50 dark:border-dark">
+                <h1 className="text-2xl dark:text-gray-50">
+                  {t('home.logicFundamentals.data.title')}
+                </h1>
+                <span className="text-md dark:text-gray-50">
+                  {t('home.logicFundamentals.data.description')}
+                </span>
               </div>
             </NavLink>
           </div>

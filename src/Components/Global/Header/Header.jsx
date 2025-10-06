321b70a5fb1005fdda14b2918ac17f7ec1f1a1ec
Guilherme-Pc <guilherme.bassani07@gmail.com>
2025-10-05 21:01:26 -0300

    terminando header

--- src/Components/Global/Header/Header.jsx
+++ src/Components/Global/Header/Header.jsx
@@ -142,7 +142,7 @@ const Header = () => {
                   ref={searchButtonRef}
                   onClick={() => setShowSearchModal(true)} 
                   aria-label={t('search.button') || "Abrir Pesquisa"}
-                  className="p-1 text-gray-700 hover:text-amarelo dark:text-gray-300 dark:hover:text-amarelo transition-colors hidden xl:block size-10"
+                  className="p-1 text-gray-700 hover:text-amarelo dark:text-gray-300 dark:hover:text-amarelo transition-colors hidden xl:block size-10 cursor-pointer"
               >
                   {/* Ícone de Lupa SVG */}
                   <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
